c8f0d068d8c6e4bd2ef50dfaeecef9b115620073
Usman Ali <imusmanali@hotmail.com>
2018-01-17 14:13:48 +0500

    1 dotween added 2 bat movement implemented

--- BrickBreaker/Assets/Scripts/PaddleTest.cs
+++ BrickBreaker/Assets/Scripts/PaddleTest.cs
@@ -1,46 +1,137 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PaddleTest : MonoBehaviour {
 
-    public bool autoPlay = false;
-    public float minX, maxX;
-
-    private Ball ball;
-
+    public DOTweenPath bat;
+    public List<Vector3> points;
+    public List<GameObject> PointsObjects;
+    public bool isMovingForward;
+    private Vector3 StartPosition;
     void Start()
     {
-        ball = GameObject.FindObjectOfType<Ball>();
+        bat = this.GetComponent<DOTweenPath>();
+        StartPosition = this.transform.position;
+        points = bat.wps;
+        PontsCreater();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!autoPlay)
+
+        Movement();
+        RotationContoller();
+
+        //if (points[0] == this.transform.position)
+        //{
+        //    if (isMovingForward)
+        //    {
+        //        Debug.Log("rotale forward");
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("rotate backword");
+        //    }
+        //}
+
+        
+    }
+
+
+    void Movement()
+    {
+        //forward
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            MoveWithMouse();
+            if (StartPosition == this.transform.position)
+            {
+                bat.DORestart();
+            }
+            bat.DOPlayForward();
+            isMovingForward = true;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            bat.DOPause();
+        }
+
+
+        //Backward
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (StartPosition == this.transform.position)
+            {
+                bat.DOComplete();
+            }
+            bat.DOPlayBackwards();
+            isMovingForward = false;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            bat.DOPause();
+        }
+    }
+    void PontsCreater()
+    {
+        int i = 0;
+        foreach (Vector3 point in points)
+        {
+            GameObject temp = new GameObject("Point" + i);
+            temp.transform.position = point;
+            BoxCollider2D tempCollider=temp.AddComponent<BoxCollider2D>();
+            tempCollider.isTrigger = true;
+            temp.layer = LayerMask.NameToLayer("PointCollision");
+            Debug.Log(temp.layer);
+            temp.AddComponent<Point>();
+            PointsObjects.Add(temp);
+            i++;
+        }
+    }
+
+    void RotationContoller()
+    {
+        if (isMovingForward)
+        {
+            if (PointsObjects[0].GetComponent<Point>().triggered == true)
+            {
+
+                transform.eulerAngles = new Vector3(0, 0, 90);
+            }
+            else if (PointsObjects[1].GetComponent<Point>().triggered == true)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 180);
+            }
+            else if (PointsObjects[2].GetComponent<Point>().triggered == true)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 270);
+            }
+            else if (PointsObjects[3].GetComponent<Point>().triggered == true)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
         }
         else
         {
-            AutoPlay();
+            if (PointsObjects[0].GetComponent<Point>().triggered == true)
+            {
+
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else if (PointsObjects[1].GetComponent<Point>().triggered == true)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 90);
+            }
+            else if (PointsObjects[2].GetComponent<Point>().triggered == true)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 180);
+            }
+            else if (PointsObjects[3].GetComponent<Point>().triggered == true)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 270);
+            }
         }
-    }
-
-    void AutoPlay()
-    {
-        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
-        Vector3 ballPos = ball.transform.position;
-        paddlePos.x = Mathf.Clamp(ballPos.x, minX, maxX);
-        this.transform.position = paddlePos;
-    }
-
-    void MoveWithMouse()
-    {
-        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
-        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-       
-        paddlePos.x = Mathf.Clamp(mousePosInBlocks, minX, maxX);
-        this.transform.position = paddlePos;
     }
 }
---
+++ BrickBreaker/Assets/Scripts/Point.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point : MonoBehaviour {
+    public bool triggered = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        triggered = true;
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        triggered = false;
+    }
+}
